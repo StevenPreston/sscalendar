4e2679e7fb642ba7ab7484c4c339c127aae4772e
Steven Preston <stevenpreston@stellar16.com>
2016-11-17 22:19:31 -0500

    Updates to Swift 3

--- Example/SSCalendar/ViewController.swift
+++ Example/SSCalendar/ViewController.swift
@@ -11,16 +11,16 @@ import SSCalendar
 
 class ViewController: UIViewController {
 
-    @IBAction func buttonTapped(sender: AnyObject?) {
+    @IBAction func buttonTapped(_ sender: AnyObject?) {
         SSStyles.applyNavigationBarStyles()
 
         let annualViewController = SSCalendarAnnualViewController(events: generateEvents())
-        let navigationController = UINavigationController(rootViewController: annualViewController)
-        navigationController.navigationBar.translucent = false
-        self.presentViewController(navigationController, animated: true, completion: nil)
+        let navigationController = UINavigationController(rootViewController: annualViewController!)
+        navigationController.navigationBar.isTranslucent = false
+        self.present(navigationController, animated: true, completion: nil)
     }
 
-    private func generateEvents() -> [SSEvent] {
+    fileprivate func generateEvents() -> [SSEvent] {
         var events: [SSEvent] = []
         for year in 2016...2021 {
             for _ in 1...200 {
@@ -30,12 +30,12 @@ class ViewController: UIViewController {
         return events
     }
 
-    private func generateEvent(year: Int) -> SSEvent {
+    fileprivate func generateEvent(_ year: Int) -> SSEvent {
         let month = Int(arc4random_uniform(12)) + 1
         let day = Int(arc4random_uniform(28)) + 1
 
         let event = SSEvent()
-        event.startDate = SSCalendarUtils.dateWithYear(year, month: month, day: day)
+        event.startDate = SSCalendarUtils.date(withYear: year, month: month, day: day)
         event.startTime = "09:00"
         event.name = "Example Event"
         event.desc = "Details of the event"
